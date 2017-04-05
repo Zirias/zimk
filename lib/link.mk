ef7de8c7dd1f7273d31fcb2f41fb358dc42e3f25
Felix Palmen <felix@palmen-it.de>
2017-04-05 10:05:42 +0200

    fix linking
     - platform-specific static libs
     - more reliable (static libs at end of command line)

--- lib/link.mk
+++ lib/link.mk
@@ -1,15 +1,30 @@
 define LINKFLAGS
 
-ifneq ($$(strip $$($(_T)_STATICLIBS)),)
-_$(_T)_LINK+=-Wl,-Bstatic $$(addprefix -l,$$($(_T)_STATICLIBS)) -Wl,-Bdynamic
-endif
+undefine _$(T)_LIBS
+undefine _$(T)_STATICLIBS
 
 ifneq ($$(strip $$($(_T)_LIBS)),)
-_$(_T)_LINK+=$$(addprefix -l,$$($(_T)_LIBS))
+_$(_T)_LIBS += $$($(_T)_LIBS)
+endif
+
+ifneq ($$(strip $$($(_T)_STATICLIBS)),)
+_$(_T)_STATICLIBS += $$($(_T)_STATICLIBS)
 endif
 
 ifneq ($$(strip $$($(_T)_$$(PLATFORM)_LIBS)),)
-_$(_T)_LINK+=$$(addprefix -l,$$($(_T)_$$(PLATFORM)_LIBS))
+_$(_T)_LIBS += $$($(_T)_$$(PLATFORM)_LIBS)
+endif
+
+ifneq ($$(strip $$($(_T)_$$(PLATFORM)_STATICLIBS)),)
+_$(_T)_STATICLIBS += $$($(_T)_$$(PLATFORM)_STATICLIBS)
+endif
+
+ifneq ($$(strip $$(_$(_T)_LIBS)),)
+_$(_T)_LINK+=$$(addprefix -l,$$(_$(_T)_LIBS))
+endif
+
+ifneq ($$(strip $$(_$(_T)_STATICLIBS)),)
+_$(_T)_LINK+=-Wl,-Bstatic $$(addprefix -l,$$(_$(_T)_STATICLIBS))
 endif
 
 endef
