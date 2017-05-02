bf2491ad97a8a6a554bcbd356a251209a8eb4f6a
Felix Palmen <felix@palmen-it.de>
2017-05-02 11:23:14 +0200

    link dynamic libs after static libs

--- lib/link.mk
+++ lib/link.mk
@@ -1,30 +1,30 @@
 define LINKFLAGS
 
-undefine _$(_T)_LIBS
 undefine _$(_T)_STATICLIBS
-
-ifneq ($$(strip $$($(_T)_LIBS)),)
-_$(_T)_LIBS += $$($(_T)_LIBS)
-endif
+undefine _$(_T)_LIBS
 
 ifneq ($$(strip $$($(_T)_STATICLIBS)),)
 _$(_T)_STATICLIBS += $$($(_T)_STATICLIBS)
 endif
 
-ifneq ($$(strip $$($(_T)_$$(PLATFORM)_LIBS)),)
-_$(_T)_LIBS += $$($(_T)_$$(PLATFORM)_LIBS)
+ifneq ($$(strip $$($(_T)_LIBS)),)
+_$(_T)_LIBS += $$($(_T)_LIBS)
 endif
 
 ifneq ($$(strip $$($(_T)_$$(PLATFORM)_STATICLIBS)),)
 _$(_T)_STATICLIBS += $$($(_T)_$$(PLATFORM)_STATICLIBS)
 endif
 
-ifneq ($$(strip $$(_$(_T)_LIBS)),)
-_$(_T)_LINK+=$$(addprefix -l,$$(_$(_T)_LIBS))
+ifneq ($$(strip $$($(_T)_$$(PLATFORM)_LIBS)),)
+_$(_T)_LIBS += $$($(_T)_$$(PLATFORM)_LIBS)
 endif
 
 ifneq ($$(strip $$(_$(_T)_STATICLIBS)),)
 _$(_T)_LINK+=-Wl,-Bstatic $$(addprefix -l,$$(_$(_T)_STATICLIBS))
+endif
+
+ifneq ($$(strip $$(_$(_T)_LIBS)),)
+_$(_T)_LINK+=-Wl,-Bdynamic $$(addprefix -l,$$(_$(_T)_LIBS))
 endif
 
 endef
