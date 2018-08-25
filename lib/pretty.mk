63d1be9c132d19d40bed3d22d0847eb152161da7
Felix Palmen <felix@palmen-it.de>
2018-08-25 12:44:25 +0200

    support termcap for colors and bold

--- lib/pretty.mk
+++ lib/pretty.mk
@@ -1,15 +1,15 @@
 ifndef NCOL
 
 ifdef TERM
-ZIMK__PRNORM := $(shell tput sgr0)
-ZIMK__PRBOLD := $(shell tput bold)
-ZIMK__PRRED := $(shell tput setaf 1)
-ZIMK__PRGREEN := $(shell tput setaf 2)
-ZIMK__PRYELLOW := $(shell tput setaf 3)
-ZIMK__PRBLUE := $(shell tput setaf 4)
-ZIMK__PRMAGENTA := $(shell tput setaf 5)
-ZIMK__PRCYAN := $(shell tput setaf 6)
-ZIMK__PRWHITE := $(shell tput setaf 7)
+ZIMK__PRNORM := $(shell tput sgr0 || tput me)
+ZIMK__PRBOLD := $(shell tput bold || tput md)
+ZIMK__PRRED := $(shell tput setaf 1 || tput AF 1)
+ZIMK__PRGREEN := $(shell tput setaf 2 || tput AF 2)
+ZIMK__PRYELLOW := $(shell tput setaf 3 || tput AF 3)
+ZIMK__PRBLUE := $(shell tput setaf 4 || tput AF 4)
+ZIMK__PRMAGENTA := $(shell tput setaf 5 || tput AF 5)
+ZIMK__PRCYAN := $(shell tput setaf 6 || tput AF 6)
+ZIMK__PRWHITE := $(shell tput setaf 7 || tput AF 7)
 else
 
 ifeq ($(OS),Windows_NT)
