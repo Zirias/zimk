e0c1a6675bed0704fef8de729aa0376ca9947596
Felix Palmen <felix@palmen-it.de>
2024-02-22 10:55:37 +0100

    silent.mk: Sanitize Windows path separator
    GNU make needs the Windows path separator (\) escaped for most purposes
    (\\). But for display, make sure this is replaced again by single
    backslash.

--- lib/silent.mk
+++ lib/silent.mk
@@ -21,24 +21,29 @@ VRM:=
 VRMDR:=
 VR:=
 else
-VCC=	@echo $(EQT)   $(ZIMK__PRBOLD)$(ZIMK__PRLCYAN)[CC]$(ZIMK__PRNORM)   $@$(EQT)
-VAS=	@echo $(EQT)   $(ZIMK__PRBOLD)$(ZIMK__PRLCYAN)[AS]$(ZIMK__PRNORM)   $@$(EQT)
-VCXX=	@echo $(EQT)   $(ZIMK__PRBOLD)$(ZIMK__PRLCYAN)[CXX]$(ZIMK__PRNORM)  $@$(EQT)
-VDEP=	@echo $(EQT)   $(ZIMK__PRBOLD)$(ZIMK__PRWHITE)[DEP]$(ZIMK__PRNORM)  $@$(EQT)
-VLD=	@echo $(EQT)   $(ZIMK__PRBOLD)$(ZIMK__PRWHITE)[LD]$(ZIMK__PRNORM)   $@$(EQT)
-VAR=	@echo $(EQT)   $(ZIMK__PRBOLD)$(ZIMK__PRLGREEN)[AR]   $@$(ZIMK__PRNORM)$(EQT)
-VRES=   @echo $(EQT)   $(ZIMK__PRBOLD)$(ZIMK__PRLCYAN)[RES]$(ZIMK__PRNORM)  $@$(EQT)
-VCCLD=	@echo $(EQT)   $(ZIMK__PRBOLD)$(ZIMK__PRLGREEN)[CCLD]$(ZIMK__PRNORM) $(ZIMK__PRGREEN)$@$(ZIMK__PRNORM)$(EQT)
-VCXLD=	@echo $(EQT)   $(ZIMK__PRBOLD)$(ZIMK__PRLGREEN)[CXLD]$(ZIMK__PRNORM) $(ZIMK__PRGREEN)$@$(ZIMK__PRNORM)$(EQT)
-VSTRP=  @echo $(EQT)   $(ZIMK__PRBOLD)$(ZIMK__PRWHITE)[STRP]$(ZIMK__PRNORM) $<$(EQT)
-VINST=	@echo $(EQT)   $(ZIMK__PRBOLD)$(ZIMK__PRLMAGENTA)[INST]$(ZIMK__PRNORM) $(ZIMK__PRMAGENTA)$(_ZIMK_0)$(ZIMK__PRNORM)$(EQT)
-VMD=	@echo $(EQT)   $(ZIMK__PRBOLD)$(ZIMK__PRWHITE)[MD]$(ZIMK__PRNORM)   $@$(EQT)
-VCFG=	@echo $(EQT)   $(ZIMK__PRBOLD)$(ZIMK__PRWHITE)[CFG]$(ZIMK__PRNORM)  $<$(EQT)
-VGEN=	@echo $(EQT)   $(ZIMK__PRBOLD)$(ZIMK__PRWHITE)[GEN]$(ZIMK__PRNORM)  $@$(EQT)
-VGIT=	@echo $(EQT)   $(ZIMK__PRBOLD)$(ZIMK__PRWHITE)[GIT]$(ZIMK__PRNORM)  $@$(EQT)
-VSUB=	@echo $(EQT)   $(ZIMK__PRBOLD)$(ZIMK__PRWHITE)[SUB]$(ZIMK__PRNORM)  $@$(EQT)
-VRM=	@echo $(EQT)   $(ZIMK__PRBOLD)$(ZIMK__PRLRED)[RM]   $(ZIMK__PRNORM)$(ZIMK__PRRED)$(_ZIMK_0)$(ZIMK__PRNORM)$(EQT)
-VRMDR=	@echo $(EQT)   $(ZIMK__PRBOLD)$(ZIMK__PRLRED)[RMDR] $(ZIMK__PRNORM)$(ZIMK__PRLRED)$(_ZIMK_0)$(ZIMK__PRNORM)$(EQT)
+ifeq ($(PSEP),\\)
+zimk__pp=$(subst \\,\,$1)
+else
+zimk__pp=$1
+endif
+VCC=	@echo $(EQT)   $(ZIMK__PRBOLD)$(ZIMK__PRLCYAN)[CC]$(ZIMK__PRNORM)   $(call zimk__pp,$@)$(EQT)
+VAS=	@echo $(EQT)   $(ZIMK__PRBOLD)$(ZIMK__PRLCYAN)[AS]$(ZIMK__PRNORM)   $(call zimk__pp,$@)$(EQT)
+VCXX=	@echo $(EQT)   $(ZIMK__PRBOLD)$(ZIMK__PRLCYAN)[CXX]$(ZIMK__PRNORM)  $(call zimk__pp,$@)$(EQT)
+VDEP=	@echo $(EQT)   $(ZIMK__PRBOLD)$(ZIMK__PRWHITE)[DEP]$(ZIMK__PRNORM)  $(call zimk__pp,$@)$(EQT)
+VLD=	@echo $(EQT)   $(ZIMK__PRBOLD)$(ZIMK__PRWHITE)[LD]$(ZIMK__PRNORM)   $(call zimk__pp,$@)$(EQT)
+VAR=	@echo $(EQT)   $(ZIMK__PRBOLD)$(ZIMK__PRLGREEN)[AR]   $(call zimk__pp,$@)$(ZIMK__PRNORM)$(EQT)
+VRES=   @echo $(EQT)   $(ZIMK__PRBOLD)$(ZIMK__PRLCYAN)[RES]$(ZIMK__PRNORM)  $(call zimk__pp,$@)$(EQT)
+VCCLD=	@echo $(EQT)   $(ZIMK__PRBOLD)$(ZIMK__PRLGREEN)[CCLD]$(ZIMK__PRNORM) $(ZIMK__PRGREEN)$(call zimk__pp,$@)$(ZIMK__PRNORM)$(EQT)
+VCXLD=	@echo $(EQT)   $(ZIMK__PRBOLD)$(ZIMK__PRLGREEN)[CXLD]$(ZIMK__PRNORM) $(ZIMK__PRGREEN)$(call zimk__pp,$@)$(ZIMK__PRNORM)$(EQT)
+VSTRP=  @echo $(EQT)   $(ZIMK__PRBOLD)$(ZIMK__PRWHITE)[STRP]$(ZIMK__PRNORM) $(call zimk__pp,$<)$(EQT)
+VINST=	@echo $(EQT)   $(ZIMK__PRBOLD)$(ZIMK__PRLMAGENTA)[INST]$(ZIMK__PRNORM) $(ZIMK__PRMAGENTA)$(call zimk__pp,$(_ZIMK_0))$(ZIMK__PRNORM)$(EQT)
+VMD=	@echo $(EQT)   $(ZIMK__PRBOLD)$(ZIMK__PRWHITE)[MD]$(ZIMK__PRNORM)   $(call zimk__pp,$@)$(EQT)
+VCFG=	@echo $(EQT)   $(ZIMK__PRBOLD)$(ZIMK__PRWHITE)[CFG]$(ZIMK__PRNORM)  $(call zimk__pp,$<)$(EQT)
+VGEN=	@echo $(EQT)   $(ZIMK__PRBOLD)$(ZIMK__PRWHITE)[GEN]$(ZIMK__PRNORM)  $(call zimk__pp,$@)$(EQT)
+VGIT=	@echo $(EQT)   $(ZIMK__PRBOLD)$(ZIMK__PRWHITE)[GIT]$(ZIMK__PRNORM)  $(call zimk__pp,$@)$(EQT)
+VSUB=	@echo $(EQT)   $(ZIMK__PRBOLD)$(ZIMK__PRWHITE)[SUB]$(ZIMK__PRNORM)  $(call zimk__pp,$@)$(EQT)
+VRM=	@echo $(EQT)   $(ZIMK__PRBOLD)$(ZIMK__PRLRED)[RM]   $(ZIMK__PRNORM)$(ZIMK__PRRED)$(call zimk__pp,$(call zimk__pp,$(_ZIMK_0)))$(ZIMK__PRNORM)$(EQT)
+VRMDR=	@echo $(EQT)   $(ZIMK__PRBOLD)$(ZIMK__PRLRED)[RMDR] $(ZIMK__PRNORM)$(ZIMK__PRLRED)$(call zimk__pp,$(_ZIMK_0))$(ZIMK__PRNORM)$(EQT)
 VR:=	@
 endif
 
